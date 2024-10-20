7123276f304ede8165620c882d057a03163a71ef
deepanshuhyb <deepanshuhyb@gmail.com>
2024-10-20 17:39:10 +0530

    setting up the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header'
+import Footer from './components/footer'
+import Search from './components/search'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
+      <Search />
+      <Footer />
     </>
   )
 }
